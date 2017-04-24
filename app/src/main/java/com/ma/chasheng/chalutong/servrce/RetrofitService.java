470af15023d95a89330e1554c059725935cf9d04
mapingan <15154175280@163.com>
2017-04-24 17:04:52 +0800

    构建

---
+++ app/src/main/java/com/ma/chasheng/chalutong/servrce/RetrofitService.java
@@ -0,0 +1,17 @@
+package com.ma.chasheng.chalutong.servrce;
+
+import java.io.File;
+
+import okhttp3.Cache;
+
+/**
+ * Created by mapingan
+ * on 2017/4/19 0019.
+ */
+
+public class RetrofitService {
+
+    public static void init(){
+
+    }
+}
